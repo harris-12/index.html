5eb50914d42bceae28a44c53dcb1907f3d6e0c04
harris-12 <52415685+harris-12@users.noreply.github.com>
2019-07-19 10:25:45 +0200

    Add files via upload

---
+++ js/onepage.js
@@ -0,0 +1,34 @@
+
+
+
+function Badsegeberg1900Click(){
+    
+  document.getElementById("badsegeberg1").classList.add("hidden");
+  document.getElementById("badsegeberg3").classList.add("hidden");
+      document.getElementById("badsegeberg4").classList.add("hidden");
+  document.getElementById("badsegeberg2").classList.remove("hidden");
+  
+  }
+  function IntroductionClick() {
+      document.getElementById("badsegeberg1").classList.remove("hidden");
+      document.getElementById("badsegeberg2").classList.add("hidden");
+      document.getElementById("badsegeberg3").classList.add("hidden");
+      document.getElementById("badsegeberg4").classList.add("hidden");
+    }
+    function badsegeberg1940Click() {
+      document.getElementById("badsegeberg1").classList.add("hidden");
+      document.getElementById("badsegeberg2").classList.add("hidden");
+      document.getElementById("badsegeberg4").classList.add("hidden");
+      document.getElementById("badsegeberg3").classList.remove("hidden");
+    }
+    function badsegeberg2019Click() {
+      document.getElementById("badsegeberg1").classList.add("hidden");
+      document.getElementById("badsegeberg2").classList.add("hidden");
+      document.getElementById("badsegeberg3").classList.add("hidden");
+      document.getElementById("badsegeberg4").classList.remove("hidden");
+    }
+
+
+
+
+   
